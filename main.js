7506d7d92ff91fcf0045fc67baa20487abece596
NathShubham28 <nathshubham69@gmail.com>
2020-11-02 07:27:50 +0530

    Interface added

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
-const {app, BrowserWindow} = require('electron') 
+const {app, BrowserWindow} = require('electron')
 const url = require('url') 
 const path = require('path')  
 
-let win  
+
+let splash
 
 function createWindow() { 
-   win = new BrowserWindow({width: 800, height: 600, frame: false}) 
-   win.loadURL(url.format ({ 
-      pathname: path.join(__dirname, 'index.html'), 
+   
+mainWindow = new BrowserWindow({
+   width: 1200,
+   height: 1000,
+   show: false
+});
+
+   splash = new BrowserWindow({width: 800, height: 600, frame: false}) 
+   splash.loadURL(url.format ({ 
+      pathname: path.join(__dirname, 'splash.html'), 
       protocol: 'file:', 
       slashes: true 
    })) 
+   mainWindow.loadURL('file://__dirname/Editor.html');
+   mainWindow.once('ready-to-show', () =>{
+      splash.destroy(5*1000);
+      mainWindow.show();
+   });   
 }  
 
 app.on('ready', createWindow) 
